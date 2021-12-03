a37c73babd99d0e8547f99fb2b907c21b313ec3a
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2021-12-03 08:19:45 +0100

    test google login

--- examples/auth-google/cypress/support/commands.ts
+++ examples/auth-google/cypress/support/commands.ts
@@ -8,8 +8,8 @@ Cypress.Commands.add("loginGoogle", () => {
   cy.visit("https://accounts.google.com/Login?hl=en");
   cy.location().then((loc) => {
     if (!loc.href.includes("https://myaccount.google.com")) {
-      cy.get("#Email").type(Cypress.env("GMAIL_EMAIL"));
-      cy.get("#next").click();
+      cy.get("#identifierId").type(Cypress.env("GMAIL_EMAIL"));
+      cy.get("#identifierNext").click();
       cy.get("#password").type(Cypress.env("GMAIL_PASSWORD"));
       cy.get("#submit").click();
     }
