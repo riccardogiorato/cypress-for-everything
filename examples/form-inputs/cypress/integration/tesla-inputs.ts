c764b8e6d8997191adae573af449f09aceaafb98
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2022-04-12 10:29:40 +0200

    update and fix tesla form-inputs

--- examples/form-inputs/cypress/integration/tesla-inputs.ts
+++ examples/form-inputs/cypress/integration/tesla-inputs.ts
@@ -11,17 +11,17 @@ describe("Tesla Newsletter Form Inputs", async function () {
 
     cy.contains("Stay Connected");
 
-    cy.get('body input[name="/firstName"]').type(`name ${testId}`);
-    cy.get('body input[name="/lastName"]').type(`name ${testId}`);
+    cy.get('body input[name="firstName"]').type(`name ${testId}`);
+    cy.get('body input[name="lastName"]').type(`name ${testId}`);
 
-    cy.get('body input[name="/email"]').type(fake_email);
+    cy.get('body input[name="email"]').type(fake_email);
 
-    cy.get('body input[name="/zip"]').type(testId.toString());
+    cy.get('body input[name="zip"]').type(testId.toString());
 
-    cy.get("fieldset input[id='/productInterested_Model S']").click();
-    cy.get("fieldset input[id='/productInterested_Model 3']").click();
-    cy.get("fieldset input[id='/productInterested_Model X']").click();
-    cy.get("fieldset input[id='/productInterested_Model Y']").click();
+    cy.get("input[name='productInterested'][value='Model S']").click();
+    cy.get("input[name='productInterested'][value='Model 3']").click();
+    cy.get("input[name='productInterested'][value='Model X']").click();
+    cy.get("input[name='productInterested'][value='Model Y']").click();
 
     cy.get('body button[type="submit"]').within(($button) => {
       cy.wrap($button).should("have.text", "Submit");
