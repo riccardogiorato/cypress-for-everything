f31e55dfd9dcd0b645233b2c1c2defc3333284e9
Riccardo Giorato <riccardo@teero.com>
2022-08-17 17:06:27 +0200

    remove jsons

---
+++ examples/auth-google/cypress.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  fixturesFolder: false,
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "../../report/[hash].xml",
+    toConsole: true,
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  screenshotOnRunFailure: false,
+  chromeWebSecurity: false,
+});
---
+++ examples/gmail/cypress.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  fixturesFolder: false,
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "../../report/[hash].xml",
+    toConsole: true,
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  screenshotOnRunFailure: false,
+  defaultCommandTimeout: 30000,
+  responseTimeout: 30000,
+  requestTimeout: 30000,
+});
