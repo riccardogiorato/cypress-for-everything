85df7873388c9af8bc11a73339107e9c53fdf80e
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-04-01 00:13:41 +0200

    fix form input

--- examples/form-inputs/cypress/e2e/tesla-inputs.cy.ts
+++ examples/form-inputs/cypress/e2e/tesla-inputs.cy.ts
@@ -3,9 +3,25 @@ describe("Tesla Newsletter Form Inputs", async function () {
   const fake_email = `fake.email.cypress4everything+${testId}@gmail.com`;
 
   it("Newsletter Form: Email is submitted from UI to server!", function () {
-    cy.visit("https://www.tesla.com/updates");
+    cy.visit("https://www.tesla.com/updates", {
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Cache-Control": "max-age=0",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
+      },
+    });
     cy.wait(2000);
-    cy.get(".tds-modal-close > .tds-icon").click({
+    cy.get(":nth-child(1) > .tds-icon-btn").click({
       force: true,
     });
 
