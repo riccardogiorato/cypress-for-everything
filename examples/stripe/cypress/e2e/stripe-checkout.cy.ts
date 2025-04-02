80ca4a563b5c018108a619a092c739921e3a5ab7
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-04-02 14:43:06 +0200

    fix stirpe checkout

--- examples/stripe/cypress/e2e/stripe-checkout.cy.ts
+++ examples/stripe/cypress/e2e/stripe-checkout.cy.ts
@@ -4,31 +4,28 @@ describe("Stripe Checkout", () => {
   });
 
   it("Stripe Checkout test mode works!", () => {
-    cy.request(
-      "https://checkout.stripe.dev/api/demo-session?country=us&billingPeriod=monthly&hasBgColor=false&bgColor=%2523ffffff&buttonColor=%2523192552&hasBillingAndShipping=false&hasCoupons=false&hasFreeTrial=false&hasShippingRate=false&hasTaxes=false&mode=payment&wallet=applePay&hasPolicies=false&billingType=flat&hasUpsells=false&hasPhoneNumber=false&borderStyle=rounded&fontStyle=System&hasCrossSells=false"
-    ).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body).to.have.property("url");
-      cy.visit(response.body.url);
-      cy.url().should("contains", "https://checkout.stripe.com/c/pay/");
+    cy.visit(
+      "https://js.stripe.com/v3/embedded-checkout-inner.html?publishableKey=pk_test_51HF0gZCJR1nkic2WnoloCeF2tR8ogJltEb028bXaSr8jDzAM4yKmFxFrbC5JiDYsKhuvqwAmJc75J1NgbrKwFO7I00L4EhhfIF&checkoutSessionId=cs_test_a1zwhUfc7lqU7V2vrPnGlKWI5UIxj6XHRyaHsjp8ty7XGGFNmrss1kXraC&ui_mode=embedded&__isDemoMode=true"
+    );
+    cy.url().should("contains", "https://js.stripe.com");
 
-      cy.get("#email").type("SatoshiNakamoto@email.com");
-      cy.get("#cardNumber").type("4242424242424242");
-      cy.get("#cardCvc").type("123");
-      cy.get("#cardExpiry").type(
-        "12" + (new Date().getFullYear() + 10).toString().substr(-2)
-      );
-      cy.get("#billingName").type("Satoshi Nakamoto");
-      cy.get("#billingPostalCode").type("94043");
+    cy.get('[data-testid="card-accordion-item"]').click();
 
-      cy.wait(1000);
-      cy.get(".SubmitButton").should(($div) => {
-        expect($div.text()).to.include("Pay");
-      });
-      cy.get(".SubmitButton").click();
-      cy.get(".SubmitButton").should(($div) => {
-        expect($div.text()).to.include("Processing");
-      });
+    cy.get("#email").type("SatoshiNakamoto@email.com");
+    cy.get("#cardNumber").type("4242424242424242");
+    cy.get("#cardCvc").type("123");
+    cy.get("#cardExpiry").type(
+      "12" + (new Date().getFullYear() + 10).toString().substr(-2)
+    );
+    cy.get("#billingName").type("Satoshi Nakamoto");
+
+    cy.wait(1000);
+    cy.get(".SubmitButton").should(($div) => {
+      expect($div.text()).to.include("Pay");
+    });
+    cy.get(".SubmitButton").click();
+    cy.get(".SubmitButton").should(($div) => {
+      expect($div.text()).to.include("Processing");
     });
   });
 });
